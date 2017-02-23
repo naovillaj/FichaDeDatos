634167fb249aa281a76fb9ea44573820e7b27ceb
Naomi Villanueva <naovillaj@gmail.com>
2017-02-23 14:51:16 -0500

    Ficha de Datos

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+
+var imprimir = document.getElementById("imprimir");
+var records= document.getElementById('records');
+imprimir.onclick = function(){
+	
+	var nombre = document.getElementById("nombre").value;
+	var apellido = document.getElementById("apellido").value;
+	var dni = document.getElementById("dni").value;
+	var direccion = document.getElementById("direccion").value;
+
+	if(nombre != "" && apellido != "" && dni != "" && direccion != ""){
+
+	records.innerHTML = "<ul><li>" + "Nombre: " + nombre +  "</li>" 
+							   + "<li>" + "Apellido: " + apellido + "</li>" 
+							   + "<li>" + "DNI: " + dni + "</li>" 
+							   + "<li>" + "Dirección: " + direccion + "</li></ul>" ;
+
+	}
+}
